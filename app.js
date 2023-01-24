5483ce0c1e7601e35821f2745d1a641e19b1221c
Julietrantt <1995julietran@gmail.com>
2023-01-24 19:18:45 +0100

    cleaning

--- app.js
+++ app.js
@@ -7,23 +7,35 @@ function playRound() {
   let playerSelection = document.getElementById("playerInput").value;
   let computerSelection = getComputerChoice();
   if (playerSelection == computerSelection) {
-    document.getElementById("answer").innerHTML = "You: " + playerSelection + "<br> Computer: " + computerSelection +"<br> Try again!"
+    document.getElementById("answer").innerHTML =
+      "You: " +
+      playerSelection +
+      "<br> Computer: " +
+      computerSelection +
+      "<br> Try again!";
   } else if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "scrissors") ||
     (playerSelection == "scrissors" && computerSelection == "rock")
   ) {
-    document.getElementById("answer").innerHTML = "You: " + playerSelection + "<br> Computer: " + computerSelection +"<br> You lose!";
+    document.getElementById("answer").innerHTML =
+      "You: " +
+      playerSelection +
+      "<br> Computer: " +
+      computerSelection +
+      "<br> You lose!";
   } else if (
     (playerSelection == "rock" && computerSelection == "scrissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scrissors" && computerSelection == "paper")
   ) {
-    document.getElementById("answer").innerHTML = "You: " + playerSelection + "<br> Computer: " + computerSelection +"<br> You Win!";
+    document.getElementById("answer").innerHTML =
+      "You: " +
+      playerSelection +
+      "<br> Computer: " +
+      computerSelection +
+      "<br> You Win!";
   }
-console.log(playerSelection)
-console.log(computerSelection)
+  console.log(playerSelection);
+  console.log(computerSelection);
 }
-
-
- 
