5943891d7ef2a3740c21242f6950054ad6247f16
Julietrantt <1995julietran@gmail.com>
2023-01-24 17:59:27 +0100

    adding result and each choices

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ function playRound() {
   let playerSelection = document.getElementById("playerInput").value;
   let computerSelection = getComputerChoice();
   if (playerSelection == computerSelection) {
-    document.getElementById("answer").innerHTML = "Try again"
+    document.getElementById("answer").innerHTML = "You: " + playerSelection + "<br> Computer: " + computerSelection +"<br> Try again!"
   } else if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "scrissors") ||
     (playerSelection == "scrissors" && computerSelection == "rock")
   ) {
-    document.getElementById("answer").innerHTML = "You lose!";
+    document.getElementById("answer").innerHTML = "You: " + playerSelection + "<br> Computer: " + computerSelection +"<br> You lose!";
   } else if (
     (playerSelection == "rock" && computerSelection == "scrissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scrissors" && computerSelection == "paper")
   ) {
-    document.getElementById("answer").innerHTML = "You Win!";
+    document.getElementById("answer").innerHTML = "You: " + playerSelection + "<br> Computer: " + computerSelection +"<br> You Win!";
   }
 console.log(playerSelection)
 console.log(computerSelection)
